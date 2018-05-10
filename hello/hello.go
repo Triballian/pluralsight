42962c29755c5ed5961ee2ae60a992d67f11960d
Triballian <email_id>
2018-05-10 02:02:06 -0500

    testing iota, increments based on number of variables inside the const declaration

--- hello/hello.go
+++ hello/hello.go
@@ -2,6 +2,21 @@ package main
 
 import "fmt"
 
+type Salutation struct {
+	name     string
+	greeting string
+}
+
+const (
+	Language = "Go"
+	A        = iota
+	B        = iota
+	C        = iota
+)
+
 func main() {
-	fmt.Println("Hello, Go")
+	var s = Salutation{}
+	s.name = "Bob"
+	fmt.Println(A, B, C)
+	fmt.Println(Language)
 }
