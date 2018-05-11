88a60936eb4a165f1d3f5dc6a8f03616937bf572
Triballian <email_id>
2018-05-11 01:18:05 -0500

    create function type to label the function instead of having an annonymous function

--- hello/hello.go
+++ hello/hello.go
@@ -7,16 +7,31 @@ type Salutation struct {
 	greeting string
 }
 
-const (
-	Language = "Go"
-	A        = iota
-	B        = iota
-	C        = iota
-)
+type Printer func(string)
+
+func Greet(salutation Salutation, do Printer) {
+	message, alternate := CreateMessage(salutation.name, salutation.greeting, "yo")
+	do(message)
+	do(alternate)
+}
+
+func CreateMessage(name string, greeting ...string) (message string, alternate string) {
+	message = greeting[1] + " " + name
+	alternate = "hey!" + name
+	return
+}
+
+func Print(s string) {
+	fmt.Print(s)
+}
+
+func PrintLine(s string) {
+	fmt.Println(s)
+}
 
 func main() {
 	var s = Salutation{}
 	s.name = "Bob"
-	fmt.Println(A, B, C)
-	fmt.Println(Language)
+	s.greeting = "Hello"
+	Greet(s, PrintLine)
 }
