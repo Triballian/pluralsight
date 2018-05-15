ee43051628399a7ed0832378fd4ac1bc4f390c75
Triballian <email_id>
2018-05-15 10:14:36 -0500

    slice of a  slice

--- hello/hello.go
+++ hello/hello.go
@@ -6,10 +6,12 @@ func main() {
 	// var s = greeting.Salutation{}
 	// s.Name = "Bob"
 	// s.Greeting = "Hello"
+
 	slice := []greeting.Salutation{
 		{"Bob", "Hello"},
 		{"Joe", "Hi"},
 		{"Mary", "What is up?"},
 	}
+	slice = slice[1:2]
 	greeting.Greet(slice, greeting.CreatePrintFunction("?"), true, 6)
 }
