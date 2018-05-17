6c93f545089fc906f806520fd0baa5e48d95de9b
Triballian <email_id>
2018-05-16 20:01:07 -0500

    Closed channel

--- hello/hello.go
+++ hello/hello.go
@@ -18,15 +18,10 @@ func main() {
 		{"Mary", "What is up?"},
 	}
 	fmt.Fprintf(&salutations[0], "The count is %d", 10)
-	done := make(chan bool, 2)
-	go func() {
-		salutations.Greet(greeting.CreatePrintFunction("<C>"), true, 6)
-		done <- true
-		done <- true
-		fmt.Println("Done!")
-	}()
-
-	salutations.Greet(greeting.CreatePrintFunction("?"), true, 6)
-	<-done //going to blok until it relceieve the varibale from the channle
-	// greeting.Greet(slice, greeting.CreatePrintFunction("?"), true, 6)
+	c := make(chan greeting.Salutation)
+	// call a goroutine that will that will fill the channel
+	go salutations.ChannelGreeter(c)
+	for s := range c {
+		fmt.Println(s.Name)
+	}
 }
